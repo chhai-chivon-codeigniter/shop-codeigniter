a0c51805f016a8a7d3ec8f2a782260f6c750e8e5
Chivon <chhaichivon@2ntkh.com>
2017-08-21 22:58:35 +0700

    dev

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: chhaichivon
+ * Date: 8/21/17
+ * Time: 10:56 PM
+ */
+class Dashboard extends CI_Controller
+{
+
+}
---
+++ application/views/dashboard/master.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chhaichivon
+ * Date: 8/21/17
+ * Time: 10:56 PM
+ */
+
+
